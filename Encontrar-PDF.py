007c0541965dab2ff510dee83da3dc2893198997
Diego <diegopc.prod@gmail.com>
2023-02-14 20:10:33 -0400

    Adição de créditos do autor.

--- Encontrar-PDF.py
+++ Encontrar-PDF.py
@@ -54,20 +54,23 @@ root = tk.Tk() # Itera o pacote Tkinter
 root.title("Encontrar PDF")
 
 # Elementos: Texto, Caixa de Texto, Botão...
-search_label = tk.Label(root, text="Encontrar PDF que contém a seguinte palavra/nome em seu conteúdo.")
+search_label = tk.Label(root, text="Encontrar PDF pela palavra-chave ou frase")
 search_entry = tk.Entry(root, width=70)
 search_entry.focus_force()
 path_button = tk.Button(root, text="Escolher pasta...", command=set_path, width=12)
 search_button = tk.Button(root, text="Pesquisar", command=on_search, width=10)
 listbox = tk.Listbox(root)
 listbox.pack()
+author_label = tk.Label(root, text="github.com/diegopereiracruz")
 listbox.bind("<Double-Button-1>", open_file) # Abrir arquivo com duplo clique
 
 # Layout
 search_label.pack(side="top", padx=(10, 0))
+author_label.pack(side="top", padx=(10, 0))
 path_button.pack(side="left", padx=(0, 0))
 search_entry.pack(side="left", padx=(10, 0), fill="x", expand=True)
 search_button.pack(side="left", padx=(10, 0))
 listbox.pack(side="bottom", fill="both", expand=True)
+
 
 root.mainloop()
